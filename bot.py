badae29bdc972448939c2dad499fd7392ef8090e
sinan33644061-lab <sinan33644061@gmail.com>
2026-05-11 10:43:30 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,11 +25,12 @@ def download_video(url):
     os.makedirs(TEMP_FOLDER, exist_ok=True)
     output_path = f"{TEMP_FOLDER}/video_%(id)s.%(ext)s"
     
-    cmd = [
+        cmd = [
         "yt-dlp",
         "-f", "best[height<=720][ext=mp4]/best[ext=mp4]/best",
         "-o", output_path,
         "--no-playlist",
+        "--cookies", "cookies.txt",  # <- ADD THIS LINE
         "--extractor-args", "youtube:player_client=android",
         "--user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
         url
